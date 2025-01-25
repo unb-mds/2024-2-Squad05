4ef5b33ee208cec6a96cf53da4e9f13a04b84180
Luiz Henrique Soares <luizhenriquesoares18@hotmail.com>
2025-01-25 14:06:07 -0300

    feat : Alterando estrutura do projeto
    - Iniciado as adaptações da estrutura do projeto para refletir um projeto Django.
    - Alterado o .gitignore
    - Alterado o nome das camadas do projeto

--- app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .view.views import *
-
-urlpatterns = [
-    path('', index, name='index'),
-]
---
+++ apps/core/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from django.db import models
+
+class Proposta(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    component_id = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    votes_count = models.IntegerField(default=0)
+    title = models.JSONField()
+    body = models.JSONField()
+    comments_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title.get("pt-BR", "Título não definido")
+
+
+class Comentario(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    commentable_type = models.CharField(max_length=255)
+    commentable_id = models.BigIntegerField()
+    author_id = models.BigIntegerField()
+    body = models.JSONField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    proposta = models.ForeignKey(Proposta, related_name="comentarios", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body.get("pt-BR", "Comentário não definido")
