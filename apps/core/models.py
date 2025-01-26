f0502deca0b9781a0a6e7a7b7067acb747e227dc
Caio Pacheco <caiopachecos29@gmail.com>
2025-01-25 22:16:37 -0300

    feat: updated models.py

--- apps/core/models.py
+++ apps/core/models.py
@@ -8,8 +8,10 @@ class Proposta(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     votes_count = models.IntegerField(default=0)
-    title = models.JSONField()
-    body = models.JSONField()
+    title = models.CharField(max_length=300)
+    title_lang = models.CharField(max_length=25)
+    body = models.CharField(max_length=10000)
+    body_lang = models.CharField(max_length=25)
     comments_count = models.IntegerField(default=0)
 
     def __str__(self):
@@ -21,10 +23,13 @@ class Comentario(models.Model):
     commentable_type = models.CharField(max_length=255)
     commentable_id = models.BigIntegerField()
     author_id = models.BigIntegerField()
-    body = models.JSONField()
+    body = models.CharField(max_length=10000)
+    body_lang = models.CharField(max_length=25)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     proposta = models.ForeignKey(Proposta, related_name="comentarios", on_delete=models.CASCADE)
+    analyzed_at = models.DateTimeField(default='2000-01-01 00:00:00.000000')
+    sentiment = models.IntegerField(default=-1)
 
     def __str__(self):
-        return self.body.get("pt-BR", "Comentário não definido")
+        return self.body
