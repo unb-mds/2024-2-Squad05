3167cb60d2316a4b428f99538a632d3cf1b71bc3
Caio Pacheco <caiopachecos29@gmail.com>
2025-01-27 20:19:35 -0300

    fix: added missing columns

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Proposta(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    component_id = models.IntegerField()
+    component_id = models.IntegerField(primary_key=True)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     votes_count = models.IntegerField(default=0)
@@ -17,17 +16,18 @@ class Proposta(models.Model):
 
 
 class Comentario(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    commentable_type = models.CharField(max_length=255)
-    commentable_id = models.BigIntegerField()
+    commentable_id = models.BigIntegerField(primary_key=True)
     author_id = models.BigIntegerField()
     body = models.CharField(max_length=10000)
     body_lang = models.CharField(max_length=25)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
+    deleted_at = models.DateTimeField()
     proposta = models.ForeignKey(Proposta, related_name="comentarios", on_delete=models.CASCADE)
-    analyzed_at = models.DateTimeField(default='2000-01-01 00:00:00.000000')
+    root_comment_id = models.BigIntegerField()
+    analyzed_at = models.DateTimeField()
     sentiment = models.IntegerField(default=-1)
+    prompt_version = models.CharField(max_length=10, default="")
 
     def __str__(self):
         return self.body
