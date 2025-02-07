247640d2b19b5aa27211a9ba5235ba8c5bffc353
gabriel <gabrielsants@gmail.com>
2025-02-07 00:07:42 -0300

    Create script populate db

---
+++ run-populate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Populate Proposta
+echo "Populate Proposta"
+docker exec lumina-web python src/manage.py import_proposta data/query_result_2024-12-09T14_15_28.94723Z.csv
+
+# Populate Comentario
+echo "Populate Comentario"
+docker exec lumina-web python src/manage.py import_comentario data/query_result_2024-12-09T16_29_01.331387Z.csv
--- src/comentario/models.py
+++ src/comentario/models.py
@@ -2,17 +2,16 @@ from django.db import models
 from proposta.models import Proposta
 
 class Comentario(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    commentable_type = models.CharField(max_length=255)
-    commentable_id = models.BigIntegerField()
-    author_id = models.BigIntegerField()
-    body = models.CharField(max_length=10000)
-    body_lang = models.CharField(max_length=25)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    proposta = models.ForeignKey(Proposta, related_name="comentarios", on_delete=models.CASCADE)
-    analyzed_at = models.DateTimeField(default='2000-01-01 00:00:00.000000')
-    sentiment = models.IntegerField(default=-1)
+    id = models.BigAutoField(primary_key=True) 
+    commentable_type = models.CharField(max_length=255) #Decidim Commentable Type
+    commentable_id = models.BigIntegerField(unique=True) # ID
+    author_id = models.BigIntegerField() # Decidim Author ID
+    body = models.CharField(max_length=10000) # Body
+    created_at = models.DateTimeField(null = True) # Created At
+    updated_at = models.DateTimeField() #  Updated At
+    proposta = models.ForeignKey(Proposta, related_name="comentarios", on_delete=models.CASCADE) #conexão com as propostas
+    analyzed_at = models.DateTimeField(default='2000-01-01 00:00:00.000000') # ??
+    sentiment = models.IntegerField(default=-1) # análise de sentimentos
 
     def __str__(self):
         return self.body
